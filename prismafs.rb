2b18d92fdc759f6e4279eb1ee6238df77565ee2e
Goran Bunic <goranb131@yahoo.com>
2024-11-19 01:15:39 +0100

    Fix tarball URL to match GitHub-generated filename

--- prismafs.rb
+++ prismafs.rb
@@ -1,7 +1,7 @@
 class Prismafs < Formula
   desc "A lightweight, layered userspace filesystem inspired by Plan 9"
   homepage "http://ithas-site.com"
-  url "https://github.com/goranb131/ITHAS-prismaFS/releases/download/v1.0.0/ITHAS-prismaFS-1.0.0.tar.gz"
+  url "https://github.com/goranb131/ITHAS-prismaFS/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "02ca028430f75e8746e3c7e476b3c4368d3c7752bd002318d11ac7d634d442b8"
   license "Apache-2.0"
 
