5eb34263e78df15ef6f205e7d04357c65ab2e52c
Goran Bunic <goranb131@yahoo.com>
2024-12-03 14:31:40 +0100

    Update Homebrew formula for PrismaFS v1.0.2

--- prismafs.rb
+++ prismafs.rb
@@ -2,7 +2,7 @@ class Prismafs < Formula
   desc "A lightweight, layered userspace filesystem inspired by Plan 9"
   homepage "http://ithas-site.com"
   url "https://github.com/goranb131/ITHAS-prismaFS/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "1225745c7a409b16708904772be01dbae015289c9b5c87e0b33a65a3e3f6c8b4"
+  sha256 "a3f7380c7c8c4f73c2cfaef3527a1a7026cbe72749e9934a624ec8655c26b4a7"
   license "Apache-2.0"
 
   def install
