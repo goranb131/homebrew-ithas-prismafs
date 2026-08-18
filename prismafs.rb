ad334e8c2be4fcb68fa9145a69c7f56465793892
Goran Bunic <goranb131@yahoo.com>
2026-08-18 19:50:24 +0200

    Formula update for ver bump 1.7.1

--- prismafs.rb
+++ prismafs.rb
@@ -1,16 +1,16 @@
 class Prismafs < Formula
   desc "A lightweight, layered userspace filesystem inspired by Plan 9"
   homepage "http://ithas-site.com"
-  url "https://github.com/goranb131/ITHAS-prismaFS/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "adb7f17a4fa6f8f1dededdf70c53e76c4fc69a6c8345c2c3597b4b23003618e9"
+  url "https://github.com/goranb131/ITHAS-prismaFS/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "684df1bc3445db6f3f997213c9984609c3af0bc5052bc184eb1a837a905726e5"
   license "Apache-2.0"
 
   def install
     # macFUSE is in /usr/local, outside homebrew on Apple Silicon.
     # Homebrew compiler strips -I/usr/local/include & -L/usr/local/lib,
     # so using CPATH/LIBRARY_PATH 
-    ENV.prepend_path "CPATH", "/usr/local/include"
-    ENV.prepend_path "LIBRARY_PATH", "/usr/local/lib"
+    ENV.append_path "CPATH", "/usr/local/include"
+    ENV.append_path "LIBRARY_PATH", "/usr/local/lib"
 
     system "make", "install", "BINDIR=#{bin}", "MANDIR=#{man1}"
   end
